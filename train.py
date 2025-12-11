cd371f502d45b14862f0101ca6c9fae3f69e1fd9
greendino <belivejs01@gmail.com>
2025-12-11 15:01:02 +0900

    ready to train

---
+++ train.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+# 첫 실행 시 자동으로 yolov8s.pt 다운로드됨
+model = YOLO('yolov8s.pt')
+
+results = model.train(
+    data='Korea-Car-License-Plate-1/data.yaml',
+    epochs=100,
+    batch=8,
+    device='mps'
+)
